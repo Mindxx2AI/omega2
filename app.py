b08a82e4cbb89a13e6284c07876ad9b3290b3a19
Mindxx2AI <mindtrival@gmail.com>
2025-03-13 17:36:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Omega2 AI Paraphrasing Tool is Running!"
+    return "Hello, Flask!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
